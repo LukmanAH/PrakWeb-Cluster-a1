446c4f9be646009d527b91d0925e184c750ebcb2
Muhammad Lukman Al Hakim <56007339+Lukboy@users.noreply.github.com>
2020-11-03 11:09:38 +0700

    update

--- Tugas_Prak1.php
+++ Tugas_Prak1.php
@@ -74,36 +74,19 @@
 <body>
   <div class="wrap">
     <div class="header">
-      <h1>Malas Ngoding</h1>
-      <p>Tutorial belajar membuat layout website sederhana</p>
+
     </div>
     <div class="menu">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">HTML</a></li>
-        <li><a href="#">CSS</a></li>
-        <li><a href="#">PHP</a></li>
-        <li><a href="#">Javascript</a></li>
-      </ul>
+
     </div>
-    <div class="badan">
-      <div class="sidebar">
-        sidebar
-        <ul>
-          <li><a href="#">Tutorial HTML dasar</a></li>
-          <li><a href="#">Tutorial CSS dasar</a></li>
-          <li><a href="#">Tutorial PHP dasar</a></li>
-          <li><a href="#">Tutorial JQuery dasar</a></li>
-        </ul>
-      </div>
-      <div class="content">
-        content
-      </div>
+    <div class="content">
+
     </div>
-    <div class="clear"></div>
-    <div class="footer">
-      footer
-    </div>
+  </div>
+  <div class="clear"></div>
+  <div class="footer">
+
+  </div>
   </div>
 </body>
 
