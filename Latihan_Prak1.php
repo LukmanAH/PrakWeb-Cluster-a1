f9ad0145001c3f7e5bfae8e08a85b30ee186351b
Muhammad Lukman Al Hakim <56007339+LukmanAH@users.noreply.github.com>
2020-11-04 16:37:15 +0700

    update dikit tapi belom kelar

--- Latihan_Prak1.php
+++ Latihan_Prak1.php
@@ -11,27 +11,30 @@
 <body>
   <div class="wrap">
     <header>
-      <h1>Malas Ngoding</h1>
-      <p>Tutorial belajar membuat layout website sederhana</p>
+      <h1>Hello, Everyone</h1>
     </header>
     <nav class="N1">
       <ul>
-        <li><a href="#menu1">Home</a></li>
-        <li><a href="#menu2">Profile</a></li>
-        <li><a href="#menu3">Contact</a></li>
+        <li><a href="#menu1">Menu1</a></li>
+        <li><a href="#menu2">Menu2</a></li>
+        <li><a href="#menu3">Menu3</a></li>
       </ul>
     </nav>
     <div class="container">
-      <section id='menu1'>
-        Selamat Datang BJirrr
+      <section id='menu1'> 
+        <h2>Ini Menu 1 Loh</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </section>
 
       <section id='menu2'>
-        Ini menu 2, Tapi belom dibikin
+      <h2>Ini Menu 2 Loh</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    
       </section>
 
       <section id='menu3'>
-        Ini menu 3, Tapi belom dibikin
+      <h2>Ini Menu 3 Loh</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </section>
     </div>
     <footer>
